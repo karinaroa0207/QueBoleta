e41d1ac17e8292d06f88933006d2eafb2f49b846
Ana <karinaroa0207@gmail.com>
2026-05-01 13:36:27 -0500

    conexion al postgres hecha ahora si

--- catalogo/catalogo/src/main/java/com/queboleta/catalogo/EventoController.java
+++ catalogo/catalogo/src/main/java/com/queboleta/catalogo/EventoController.java
@@ -1,11 +1,14 @@
 package com.queboleta.catalogo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+
+import com.queboleta.catalogo.model.Evento;
 
 @RestController
 @RequestMapping("/api/eventos")
--- catalogo/catalogo/src/main/java/com/queboleta/catalogo/EventoRepository.java
+++ catalogo/catalogo/src/main/java/com/queboleta/catalogo/EventoRepository.java
@@ -2,5 +2,7 @@ package com.queboleta.catalogo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.queboleta.catalogo.model.Evento;
+
 public interface EventoRepository extends JpaRepository<Evento, Long> {
 }
